782ea290b8b3e668ec39dbc56fe568da8f1cd308
janhavi19 <janhavi.puranik1995@gmail.com>
2018-11-04 15:58:07 +0100

    adding customer agent

---
+++ src/main/java/org/jsw/agents/CustomerAgent.java
@@ -0,0 +1,24 @@
+package java;
+
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.basic.Action;
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.domain.FIPANames;
+import jade.domain.JADEAgentManagement.JADEManagementOntology;
+import jade.domain.JADEAgentManagement.ShutdownPlatform;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import java.util.List;
+import java.util.Vector;
+import java.util.Random;
+
+public class CustomerAgent extends Agent {
+
+}
