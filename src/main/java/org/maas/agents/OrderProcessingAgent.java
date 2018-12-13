48707e140572fc60138f85f1acf5e55199f91acd
widya <widya.aulia280988@gmail.com>
2018-12-13 21:11:21 +0100

    move all function needed by OrderProcessingAgent.java from Data.java

--- src/main/java/org/maas/agents/OrderProcessingAgent.java
+++ src/main/java/org/maas/agents/OrderProcessingAgent.java
@@ -16,6 +16,7 @@ import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -32,14 +33,13 @@ import java.util.Random;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.maas.utils.Data;
 
 @SuppressWarnings("serial")
 public class OrderProcessingAgent extends BaseAgent {
+	private JSONArray dataArray = new JSONArray();
 	private List<String> productType;
 	private List<String> productPrice;
 	private String bakeryID;
-	private Data seller = new Data();
 	private int n = 0;
 	
     protected void setup() {
@@ -50,10 +50,10 @@ public class OrderProcessingAgent extends BaseAgent {
     	
     	register("OrderProcessing", bakeryID);
     	
-    	seller.retrieve("src/main/resources/config/small/bakeries.json");
+    	retrieve("src/main/resources/config/small/bakeries.json");
     	
     	Map<String,List<String>> map = new HashMap();
-    	map = seller.getProduct(bakeryID);
+    	map = getProduct(bakeryID);
     	
     	productType = map.get("productType");
     	productPrice = map.get("productPrice");
@@ -88,8 +88,20 @@ public class OrderProcessingAgent extends BaseAgent {
     						//System.out.println(bakeryName + " receive request: " + message.getContent());
     						incomingOrder = new JSONObject(message.getContent());
     						
-    						proposal = seller.checkAvailability(incomingOrder, bakeryID, productType, productPrice);
-    						
+    						proposal = checkAvailability(incomingOrder, bakeryID, productType, productPrice);
+    				
+    						//Reply the CFP with a proposal
+    	                	ACLMessage reply = message.createReply();
+    	                    reply.setPerformative(ACLMessage.PROPOSE);
+    	                    //reply.setContent("Got your order.");
+    	    				reply.setLanguage("JSON");
+    	    				reply.setContent(proposal.toString());
+    	    				sendMessage(reply);
+    	    				//System.out.println("sent reply");
+    	    				
+    	    				//System.out.println(bakeryName + " send proposal: " + proposal.toString());
+    	    				
+    	    				n++;
     					} catch (JSONException e) {
     						System.out.println("fail to get content");
     						e.printStackTrace();
@@ -97,20 +109,9 @@ public class OrderProcessingAgent extends BaseAgent {
     				} else {
     					System.out.println("message is not JSON");
     				}
-                	
-                	//Reply the CFP with a proposal
-                	ACLMessage reply = message.createReply();
-                    reply.setPerformative(ACLMessage.PROPOSE);
-                    //reply.setContent("Got your order.");
-    				reply.setLanguage("JSON");
-    				reply.setContent(proposal.toString());
-    				sendMessage(reply);
-    				//System.out.println("sent reply");
-    				
-    				//System.out.println(bakeryName + " send proposal: " + proposal.toString());
-    				
-    				n++;
     			}
+    		} else {
+    			block();
     		}
     		
     		finished();
@@ -131,4 +132,97 @@ public class OrderProcessingAgent extends BaseAgent {
         System.out.println(bakeryID + " receive " + n + " order");
         System.out.println("\t"+bakeryID+" terminating.");
     }
+    
+    public void retrieve(String fileName) {
+		File file = new File(fileName);
+		String filePath = file.getAbsolutePath();
+		String fileContent = "";	
+		
+		try {
+			fileContent = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+			dataArray = new JSONArray(fileContent);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+    
+    public JSONObject checkAvailability(JSONObject order, String bakeryName, 
+    		List<String> sellType, List<String> sellPrice) {
+    		JSONObject orderProduct = new JSONObject();
+    		JSONObject orderPrice = new JSONObject();
+    		JSONObject bakeryPrice = new JSONObject();
+    		List<String> orderType = new ArrayList();
+    		String orderID = "";		
+    		
+    		//Get All Order Type
+    		try {
+    			orderID = order.getString("guid");
+    			bakeryPrice.put("guid", orderID);
+    			
+    			orderProduct = order.getJSONObject("products");
+    			
+    			Iterator iter = orderProduct.keys();
+    			while(iter.hasNext()){
+    				String key = (String)iter.next();
+    				orderType.add(key);
+    			}
+    		} catch (JSONException e) {
+    			e.printStackTrace();
+    		}
+    		
+    		//Compare Order Type with Sell Type
+    		try {
+    			for (int i = 0; i < orderType.size(); i++) {
+    				for (int j = 0; j < sellType.size(); j++) {
+    					if (orderType.get(i).equals(sellType.get(j))) {
+    						orderPrice.put(sellType.get(j), sellPrice.get(j));
+    						break;
+    					}
+    				}
+    			}
+    			bakeryPrice.put("products", orderPrice);
+    		} catch (JSONException e) {
+    			e.printStackTrace();
+    		}
+    		
+    		//System.out.println("Bakery Price: " + bakeryPrice);
+    		
+    		return bakeryPrice;		
+    	}
+    	
+    public Map<String, List<String>> getProduct(String id) {
+    	String bakeryID = "";
+		JSONArray products = new JSONArray();
+		
+		List<String> productType = new ArrayList();
+		List<String> productPrice = new ArrayList();
+		
+		//Get Product List
+		try {
+			for (int i = 0; i < dataArray.length(); i++) {
+				bakeryID = dataArray.getJSONObject(i).getString("guid");
+				
+				if (bakeryID.equals(id)) {
+					products = dataArray.getJSONObject(i).getJSONArray("products");
+					
+					for (int j = 0; j < products.length(); j++) {
+						productType.add(products.getJSONObject(j).getString("guid"));
+						//productPrice.add(BigDecimal.valueOf(products.getJSONObject(i).getDouble("salesPrice")).floatValue());
+						productPrice.add(Double.toString(products.getJSONObject(j).getDouble("salesPrice")));
+					}
+					
+					Map<String,List<String>> map = new HashMap();
+					map.put("productType", productType);
+					map.put("productPrice", productPrice);
+					return map;
+				}
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+    }	
 }
